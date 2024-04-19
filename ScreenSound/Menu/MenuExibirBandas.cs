ae28cf475fd24198fef413e8ea2f027b4193c93a
Danillo <danillordm@gmail.com>
2024-04-19 19:00:43 -0300

    Add MenuExibirBandas

---
+++ ScreenSound/Menu/MenuExibirBandas.cs
@@ -0,0 +1,18 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Menu;
+
+internal class MenuExibirBandas : Menu
+{
+    public MenuExibirBandas(Dictionary<string, Banda> bandasRegistradas) : base(bandasRegistradas)
+    {
+        Console.Clear();
+        ExibirTitulo("Exibindo bandas registradas");
+        foreach (var nomeBanda in BandasRegistradas.Keys)
+        {
+            Console.WriteLine($"Banda: {nomeBanda}");
+        }
+
+        VoltarMenu();
+    }
+}

8ac289647ca685d37d6d2ef4328f45b414257426
Danillo <danillordm@gmail.com>
2024-04-20 16:42:40 -0300

    Adiciona Start.cs

---
+++ ScreenSound/Outros/Start.cs
@@ -0,0 +1,33 @@
+﻿// Screen Sound
+
+using ScreenSound.Menu;
+using ScreenSound.Modelos;
+
+namespace ScreenSound.Outros;
+
+internal class Start
+{
+     public Start ()
+     {
+        Dictionary<string, Banda> bandasRegistradas = [];
+        bandasRegistradas.Add("Linkin Park", new Banda("Linkin Park"));
+        bandasRegistradas["Linkin Park"].AdicionarNota(new Avaliacao(10));
+        bandasRegistradas["Linkin Park"].AdicionarNota(new Avaliacao(9));
+        bandasRegistradas["Linkin Park"].AdicionarNota(new Avaliacao(9.5f));
+
+        bandasRegistradas.Add("Slipknot", new("Slipknot"));
+        bandasRegistradas.Add("System of a Down", new("System of a Down"));
+        bandasRegistradas.Add("Coldplay", new("Coldplay"));
+
+
+        MenuExibirOpcoes menuExibirOpcoes = new(bandasRegistradas);
+        menuExibirOpcoes.ExibirOpcoesMenu();
+    }
+
+
+
+
+
+
+
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -1,22 +1,3 @@
-﻿// Screen Sound
+﻿using ScreenSound.Outros;
 
-
-using ScreenSound.Menu;
-using ScreenSound.Modelos;
-
-
-Dictionary <string, Banda> bandasRegistradas = [];
-bandasRegistradas.Add("Linkin Park", new Banda("Linkin Park"));
-bandasRegistradas["Linkin Park"].AdicionarNota(new Avaliacao(10));
-bandasRegistradas["Linkin Park"].AdicionarNota(new Avaliacao(9));
-bandasRegistradas["Linkin Park"].AdicionarNota(new Avaliacao(9.5f));
-
-bandasRegistradas.Add("Slipknot", new("Slipknot"));
-bandasRegistradas.Add("System of a Down", new("System of a Down"));
-bandasRegistradas.Add("Coldplay", new("Coldplay"));
-
-
-MenuExibirOpcoes menuExibirOpcoes = new(bandasRegistradas);
-
-menuExibirOpcoes.ExibirOpcoesMenu();
-
+Start _ = new();

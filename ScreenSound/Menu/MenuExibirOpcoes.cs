ca359578016a373bde8ec9766843503555ff7fc6
Danillo <danillordm@gmail.com>
2024-04-20 14:01:03 -0300

    Add MenuExibirOpcoes

---
+++ ScreenSound/Menu/MenuExibirOpcoes.cs
@@ -0,0 +1,52 @@
+﻿using ScreenSound.Modelos;
+
+namespace ScreenSound.Menu;
+
+internal class MenuExibirOpcoes(Dictionary<string, Banda> bandasRegistradas) : Menu(bandasRegistradas)
+{
+    public void ExibirOpcoesMenu()
+    {
+        //ExibirLogo();
+        Console.WriteLine("\nDigite 1 para registrar uma banda");
+        Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
+        Console.WriteLine("Digite 3 para mostrar todas as bandas");
+        Console.WriteLine("Digite 4 para avaliar uma banda");
+        Console.WriteLine("Digite 5 para exibir os detalhes de uma banda");
+        Console.WriteLine("Digite -1 para sair");
+
+        Console.Write("\nDigite a sua opção: ");
+
+        if (int.TryParse(Console.ReadLine()!, out int opcaoEscolhida))
+        {
+            switch (opcaoEscolhida)
+            {
+                case 1:
+                    MenuRegistrarBanda _1 = new(BandasRegistradas);
+                    break;
+                case 2:
+                    MenuRegistrarAlbum _2 = new(BandasRegistradas);
+                    break;
+                case 3:
+                    MenuExibirBandas _3 = new(BandasRegistradas);
+                    break;
+                case 4:
+                    MenuAvaliarBanda _4 = new(BandasRegistradas);
+                    break;
+                case 5:
+                    MenuExibirDetalhes _5 = new(BandasRegistradas);
+                    break;
+                case -1:
+                    Console.WriteLine("Tchau tchau :)");
+                    break;
+                default:
+                    Console.WriteLine("Opção inválida");
+                    break;
+            }
+        }
+        else
+        {
+            ExibirOpcoesMenu();
+        }
+
+    }
+}
--- ScreenSound/Program.cs
+++ ScreenSound/Program.cs
@@ -35,52 +35,7 @@ Boas vindas ao Screen Sound
 
 }
 
-void ExibirOpcoesMenu()
-{
-    ExibirLogo();
-    Console.WriteLine("\nDigite 1 para registrar uma banda");
-    Console.WriteLine("Digite 2 para registrar o álbum de uma banda");
-    Console.WriteLine("Digite 3 para mostrar todas as bandas");
-    Console.WriteLine("Digite 4 para avaliar uma banda");
-    Console.WriteLine("Digite 5 para exibir os detalhes de uma banda");
-    Console.WriteLine("Digite -1 para sair");
+MenuExibirOpcoes menuExibirOpcoes = new(bandasRegistradas);
 
-    Console.Write("\nDigite a sua opção: ");
-
-    if (int.TryParse(Console.ReadLine()!, out int opcaoEscolhida))
-    {
-        switch (opcaoEscolhida)
-        {
-            case 1:
-                MenuRegistrarBanda _1 = new(bandasRegistradas);
-                break;
-            case 2:
-                MenuRegistrarAlbum _2 = new(bandasRegistradas);
-                break;
-            case 3:
-                MenuExibirBandas _3 = new(bandasRegistradas);
-                break;
-            case 4:
-                MenuAvaliarBanda _4 = new(bandasRegistradas);
-                break;
-            case 5:
-                MenuExibirDetalhes _5 = new(bandasRegistradas);
-                break;
-            case -1:
-                Console.WriteLine("Tchau tchau :)");
-                break;
-            default:
-                Console.WriteLine("Opção inválida");
-                break;
-        }
-    }
-    else
-    {
-        ExibirOpcoesMenu();
-    }
-    
-
-}
-
-ExibirOpcoesMenu();
+menuExibirOpcoes.ExibirOpcoesMenu();
 
